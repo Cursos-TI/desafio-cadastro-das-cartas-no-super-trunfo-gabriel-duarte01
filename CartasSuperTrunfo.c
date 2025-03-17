3a137757054dee972e4403088a71d773e7188a37
gabriel-duarte01 <vitorgabriel.contato2@gmail.com>
2025-03-17 16:59:18 +0000

    Conclusão do desfaio 1 e 2 do SAVA (não enviei primeiro o desafio 1, pois o site de repositório estava com problema, então estava criando em off no próprio VS studio)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,94 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+char estado1;    
+int codigo_da_carta1;
+char cidade1 [20]; 
+int populacao1;
+float area1;
+float numero_pontos_turisticos1;
+float pib1;
+float densidade_demografica1;
+float pib_per_capita1;
 
-    return 0;
+
+char estado2;
+int codigo_da_carta2;
+char cidade2 [20];
+int populacao2;
+float area2;
+float numero_pontos_turisticos2;
+float densidade_demografica2;
+float pib2;
+
+
+
+//Área deve ser em km²
+
+//Carta 1
+
+printf("Digite o estado da carta 1: ");
+scanf("%c", &estado1);
+
+printf("Digite o código da carta 1: ");
+scanf("%d", &codigo_da_carta1);
+
+printf("Digite a cidade da carta 1: ");
+scanf("%s", cidade1);
+
+printf("Digite a população da carta 1:  ");
+scanf("%d", &populacao1);
+
+printf("Digite a área da carta 1:  ");
+scanf("%f", &area1);
+
+printf("Digite o número de pontos turísticos da carta 1:  ");
+scanf("%f", &numero_pontos_turisticos1);
+
+printf("Digite o PIB da cidade 1:  ");
+scanf("%f", &pib1);
+
+densidade_demografica1 =  populacao1/area1;
+pib_per_capita1 =  pib1/populacao1;
+
+printf("A desnidade demográfica da cidade 1 é:  %.2f \n", densidade_demografica1);
+
+printf("O PIB per capita da cidade 1 é:  %.2f \n", pib_per_capita1);
+
+//Carta 2
+
+printf("Digite o estado da carta 2: ");
+scanf(" %c", &estado2); //espaço para descartar "\n" do buffer
+
+printf("Digite o código da carta 2: ");
+scanf("%d", &codigo_da_carta2);
+
+printf("Digite a cidade da carta 2: ");
+scanf("%s", cidade2);
+
+printf("Digite a população da carta 2: ");
+scanf("%d", &populacao2);
+
+printf("Digite a área da carta 2: ");
+scanf("%f", &area2);
+
+printf("Digite o número de pontos turísticos da carta 2: ");
+scanf("%f", &numero_pontos_turisticos2);
+
+printf("Digite o PIB da cidade 2: ");
+scanf("%f", &pib2);
+
+densidade_demografica2 =  populacao2/area2;
+pib2 =  pib2/populacao2;
+
+printf("A densidade demográfica da cidade 2 é: %.2f \n", densidade_demografica2);
+
+printf("O PIB per capita da cidade 2 é: %.2f \n", pib2);
+
+
+return 0;
+
+
+
 }
